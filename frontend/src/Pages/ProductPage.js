d7ee9e06c84e544fd54454aee5d85fdd659d316b
DanielBedrack <danielbedrack@gmail.com>
2023-05-12 23:51:38 +0300

    development of ProductPage -  adding Bottstrap elements

--- frontend/src/Pages/ProductPage.js
+++ frontend/src/Pages/ProductPage.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import React, { useEffect, useReducer } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
+import ListGroup from 'react-bootstrap/esm/ListGroup';
+import Card from 'react-bootstrap/esm/Card';
+import Button from 'react-bootstrap/esm/Button';
 import { useParams } from 'react-router-dom';
+import Rating from '../Components/Rating';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -56,8 +60,58 @@ const ProductPage = () => {
                 alt={product.name}
               />
             </Col>
-            <Col md={3}></Col>
-            <Col md={3}></Col>
+            <Col md={3}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  {/* <Helmet>
+                    <title>{product.name}</title>
+                  </Helmet> */}
+                  <h1>{product.name}</h1>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  ></Rating>
+                </ListGroup.Item>
+                <ListGroup.Item>Pirce : ${product.price}</ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <Card.Body>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Price:</Col>
+                        <Col>${product.price}</Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                          {product.countInStock > 0 ? (
+                            <Badge bg="success">In Stock</Badge>
+                          ) : (
+                            <Badge bg="danger">Unavailable</Badge>
+                          )}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    {product.countInStock > 0 && (
+                      <ListGroup.Item>
+                        <div className="d-grid">
+                          <Button onClick={addToCartHandler} variant="primary">
+                            Add to Cart
+                          </Button>
+                        </div>
+                      </ListGroup.Item>
+                    )}
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
           </Row>
         </div>
       )}
