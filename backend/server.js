7077d117fc9e71e31c6796953496f16ea37b4804
DanielBedrack <danielbedrack@gmail.com>
2023-06-19 09:54:46 +0300

    uploading to render

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join('backend', 'build')));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 });
 // Define the catch-all route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join('backend', 'build', 'index.html'));
 });
 
 
